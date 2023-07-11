08b24fd20ac7fc070e2c5bc74dde2213835f4ddf
samehbenamor <sameh.benamor@esprit.tn>
2023-07-11 22:02:08 +0200

    Added more animation to work section

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,27 +5,33 @@ import { useState, useEffect } from "react";
 import Table from "react-bootstrap/Table";
 import ggaming from "../assets/img/ggaming.PNG";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import React from "react";
 export const Work = () => {
-
-  const Typewriter = ({ text, delay }) => {
-    const [currentText, setCurrentText] = useState("");
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    useEffect(() => {
-      if (currentIndex < text.length) {
-        const timeout = setTimeout(() => {
-          setCurrentText((prevText) => prevText + text[currentIndex]);
-          setCurrentIndex((prevIndex) => prevIndex + 1);
-        }, delay);
-
-        return () => clearTimeout(timeout);
-      }
-    }, [currentIndex, delay, text]);
-
-    return <span>{currentText}</span>;
+  const tdVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+  const rowVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        staggerChildren: 0.2, // Increase the duration for slower animation
+        duration: 1.5,
+        delay: 0.2, // Delay before the transition starts for each td element
+        ease: "easeInOut", // Easing function for the transition
+        type: "spring", // Animation type (tween, spring, keyframes, etc.)
+        stiffness: 25, // Damping effect for spring animations
+      },
+    },
   };
 
+  const [ref, inView] = useInView({
+    triggerOnce: true, // Trigger animation only once when entering viewport
+    threshold: 0.1, // Adjust threshold as needed
+  });
 
   const hiddenMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)`;
   const visibleMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)`;
@@ -116,22 +122,30 @@ export const Work = () => {
       };
       return (
         <motion.div
-        className="cursor-image"
-        style={cursorImageStyle}
-        initial={false}
-        animate={
-          isLoaded 
-            ? { WebkitMaskImage: visibleMask, maskImage: visibleMask, ease: "easeIn"}
-            : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask, ease: "easeIn" }
-        }
-        transition={{ duration: 0.5, delay: 0 }}
-       
-        
-      >
-      
-        <img src={hoveredImage} alt="Cursor Image" onLoad={() => setIsLoaded(true)}/>
-      </motion.div>
-       
+          className="cursor-image"
+          style={cursorImageStyle}
+          initial={false}
+          animate={
+            isLoaded
+              ? {
+                  WebkitMaskImage: visibleMask,
+                  maskImage: visibleMask,
+                  ease: "easeIn",
+                }
+              : {
+                  WebkitMaskImage: hiddenMask,
+                  maskImage: hiddenMask,
+                  ease: "easeIn",
+                }
+          }
+          transition={{ duration: 0.5, delay: 0 }}
+        >
+          <img
+            src={hoveredImage}
+            alt="Cursor Image"
+            onLoad={() => setIsLoaded(true)}
+          />
+        </motion.div>
       );
     }
     return null;
@@ -143,46 +157,59 @@ export const Work = () => {
       id="work"
       style={{ backgroundColor, transition: "background-color 1s ease-in-out" }}
     >
+    
       <div className="container">
         <div className="row">
           <div className="col-12">
             <div className="skill-bx wow zoomIn">
-              <motion.div
-              
-              
-              >
-              <h2 className="past">Past Projects</h2>
+              <motion.div>
+                <h2 className="past">Past Projects</h2>
               </motion.div>
-              
-              
+
               <Table className="projectsTable">
                 <thead>
-                  <tr
+                  <motion.tr
                     className="hoverable-tr"
                     onMouseEnter={() => handleMouseEnter(ggaming)}
                     onMouseLeave={handleMouseLeave}
                     onMouseMove={handleMouseMove}
+                    ref={ref}
+                    initial="hidden"
+                    animate={inView ? "visible" : "hidden"}
+                    variants={rowVariants}
                   >
-                    <td>2023</td>
-                    <td>Ggaming</td>
-                    <td>Studies project</td>
-                    <td>Symfony, Javafx</td>
-                    <td>Website, desktop app</td>
-                  </tr>
-                  <tr className="hoverable-tr">
-                    <td>2022</td>
-                    <td>GreenT</td>
-                    <td>Studies project</td>
-                    <td>Php</td>
-                    <td>Website</td>
-                  </tr>
-                  <tr className="hoverable-tr">
-                    <td>2022</td>
-                    <td>Smart TechStore</td>
-                    <td>Studies project</td>
-                    <td>Qt, c++</td>
-                    <td>Desktop app</td>
-                  </tr>
+                    <motion.td variants={tdVariants}>2023</motion.td>
+                    <motion.td variants={tdVariants}>Ggaming</motion.td>
+                    <motion.td variants={tdVariants}>Studies project</motion.td>
+                    <motion.td variants={tdVariants}>Symfony, Javafx</motion.td>
+                    <motion.td variants={tdVariants}>
+                      Website, desktop app
+                    </motion.td>
+                  </motion.tr>
+                  <motion.tr
+                    ref={ref}
+                    initial="hidden"
+                    animate={inView ? "visible" : "hidden"}
+                    variants={rowVariants}
+                  >
+                    <motion.td variants={tdVariants}>2022</motion.td>
+                    <motion.td variants={tdVariants}>GreenT</motion.td>
+                    <motion.td variants={tdVariants}>Studies project</motion.td>
+                    <motion.td variants={tdVariants}>Php</motion.td>
+                    <motion.td variants={tdVariants}>Website</motion.td>
+                  </motion.tr>
+                  <motion.tr
+                    ref={ref}
+                    initial="hidden"
+                    animate={inView ? "visible" : "hidden"}
+                    variants={rowVariants}
+                  >
+                    <motion.td variants={tdVariants}>2022</motion.td>
+                    <motion.td variants={tdVariants}>Smart TechStore</motion.td>
+                    <motion.td variants={tdVariants}>Studies project</motion.td>
+                    <motion.td variants={tdVariants}>Qt, c++</motion.td>
+                    <motion.td variants={tdVariants}>Desktop app</motion.td>
+                  </motion.tr>
                 </thead>
                 {renderCursorImage()}
               </Table>
@@ -190,8 +217,7 @@ export const Work = () => {
           </div>
         </div>
       </div>
-      <img className="background-image-left" src={""} alt="Image" />
+
     </section>
   );
-}
-
+};
